491b364b7dc8cdd996069311eef3fabcb7299bb6
Vincent Zeng <ykzeng@outlook.com>
2022-07-29 22:38:58 -0500

    automation script for training

---
+++ train.sh
@@ -0,0 +1,29 @@
+new_prefix=$1
+pretrained_prefix=$2
+time=$3
+# EXAMPLE:
+# new_prefix=lw2_on1_on5_vz1
+# pretrained_prefix=lw2_on1_on5
+# time=70
+
+id="${new_prefix//_/-}"
+echo "id to be plugged in new training job:"
+echo $id
+
+cp run.env.orig run.env
+echo "DR_LOCAL_S3_MODEL_PREFIX=$new_prefix">>run.env
+echo "DR_LOCAL_S3_PRETRAINED_PREFIX=$pretrained_prefix">>run.env
+echo "DR_LOCAL_S3_PRETRAINED_CHECKPOINT=best">>run.env
+
+echo "============run.env file content============"
+cat run.env
+echo "============run.env file content============"
+
+cd ..
+echo "current directory:"
+pwd
+
+# ./create-standard-instance.sh DFWDR-blackbird71-training-stack-1 DFWDR-blackbird71-training-job-${id} $time
+echo "./create-standard-instance.sh DFWDR-blackbird71-training-stack-1 DFWDR-blackbird71-training-job-${id} $time"
+./create-standard-instance.sh DFWDR-blackbird71-training-stack-1 DFWDR-blackbird71-training-job-${id} $time
+#  &> logs/train-${id}.log &
